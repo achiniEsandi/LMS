524fbb50039932b4fcf0481ece4fabcbd9a92440
Esandi de Silva <esandidesilva2003@gmail.com>
2025-11-29 16:08:08 +0530

    CreatingReactApp

---
+++ asgardeo-login-demo/src/authConfig.js
@@ -0,0 +1,7 @@
+export const config = {
+    signInRedirectURL: "http://localhost:3000",
+    signOutRedirectURL: "http://localhost:3000",
+    clientID: "YOUR_CLIENT_ID",
+    baseUrl: "https://api.asgardeo.io/t/your-org-id",
+    scope: ["openid", "profile", "email"]
+};
